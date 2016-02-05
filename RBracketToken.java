faf6e2ce7b9988f864e5d120f5b6fe7a0bfe3d78
joemarch01 <jm360@st-andrews.ac.uk>
2016-02-05 13:27:40 +0000

    Create RBracketToken.java

---
+++ RBracketToken.java
@@ -0,0 +1,15 @@
+public class RBracketToken extends Token {
+    private static int numberOfRBrackets = 0;
+
+    public static int getNumberOfRBrackets () {
+        return numberOfRBrackets;
+    }
+
+    public static void resetNumberOfRBrackets () {
+        numberOfRBrackets = 0;
+    }
+
+    public RBracketToken () {
+        numberOfRBrackets ++;
+    }
+}
